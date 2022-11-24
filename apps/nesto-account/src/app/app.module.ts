3dd71c33d216a7aaecbfe3107c8fc166bbd752a2
phong cao <phongcao3091998@gmail.com>
2022-11-25 01:19:04 +0700

    feat: init cart account login

--- apps/nesto-account/src/app/app.module.ts
+++ apps/nesto-account/src/app/app.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AccountInformationComponent } from './routes/account-information/account-information.component';
+import { CartComponent } from './routes/cart/cart.component';
+import { LoginComponent } from './routes/login/login.component';
+
+export const ROUTES: Route[] = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'account-information',
+    component: AccountInformationComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  }
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    CartComponent,
+    AccountInformationComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ apps/nesto-account/src/app/routes/account-information/account-information.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mix-account-information',
+  templateUrl: './account-information.component.html',
+  styleUrls: ['./account-information.component.scss']
+})
+export class AccountInformationComponent {}
